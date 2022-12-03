42f69c9d8cbdef7c9ad31ceb8e94d670b0a3fa1b
quite <quitetaken@gmx.de>
2022-12-03 13:50:17 +0100

    implement attack
    The weapon now rotates towards clicks.

--- Assets/Scripts/WeaponBehavior.cs
+++ Assets/Scripts/WeaponBehavior.cs
@@ -22,20 +22,23 @@ public class WeaponBehavior : MonoBehaviour
         // Left Mouse button click
         if(Input.GetButtonDown("Fire1") && !_attacking)
         {
-            Debug.Log("Swing!");
             Vector2 mousePosition = Input.mousePosition;
+            // Find the point in game we clicked at
             Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-            // playerWeapon.transform.LookAt(mouseWorldPosition, Vector3.up);
+            // Rotate the weapon to point into clicked direction
+            playerWeapon.transform.LookAt(mouseWorldPosition, Vector3.forward);
+
+            // Do the attack 
             GetComponent<Renderer>().enabled = true;
             _attacking = true;
-
             StartCoroutine(SwingAnimation());
         }
     }
 
     #region IEnumerators
 
+        // Do a single attack
         private IEnumerator SwingAnimation()
         {
             yield return new WaitForSeconds(attackDuration);
